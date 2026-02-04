30d8e7216da9dc1f08338220b6b2dc90277d27fb
juliamarcus15 <jrmarcus@berkeley.edu>
2026-02-04 15:59:37 -0800

    Add files via upload

--- labactivity1.py
+++ labactivity1.py
@@ -9,12 +9,16 @@ bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 # change this to match the location's pressure (hPa) at sea level
 bme680.sea_level_pressure = 1013.25
 
-while True:
-    print("\nTemperature: %0.1f C" % bme680.temperature)
-    print("Gas: %d ohm" % bme680.gas)
-    print("Humidity: %0.1f %%" % bme680.relative_humidity)
-    print("Pressure: %0.3f hPa" % bme680.pressure)
-    print("Altitude = %0.2f meters" % bme680.altitude)
 
-    time.sleep(2)
+itime=time.time()
+stop=itime+10
+while itime < stop:
+    itime = time.time()
+    print("%0.1f C, " % bme680.temperature, "%d ohm, " % bme680.gas, "%0.1f %%, " % bme680.relative_humidity, "%0.3f hPa, " % bme680.pressure, "%0.2f meters, " % bme680.altitude, "%0.2f s" %itime)
+   # print("Gas: %d ohm" % bme680.gas)
+  #  print("Humidity: %0.1f %%" % bme680.relative_humidity)
+  #  print("Pressure: %0.3f hPa" % bme680.pressure)
+   # print("Altitude = %0.2f meters" % bme680.altitude)
+  #  print( i, end = "Temperature","Gas","Humidity", "Pressure"," Altitude")
+    time.sleep(1)
     
