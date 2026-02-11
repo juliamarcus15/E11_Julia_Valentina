3b7920682cf0ff1dcdcc2cde03c769741e145cba
juliamarcus15 <jrmarcus@berkeley.edu>
2026-02-11 15:26:48 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import csv
+import time
+import numpy as np
+file=open('test.csv','w',newline=None)
+csvwriter=csv.writer(file,delimiter=',')
+meta=['time','data']
+csvwriter.writerow(meta)
+for i in range(10):
+    now = time.time()
+    value = np.random.random()
+    csvwriter.writerow([now,value])
+
+file.close()
+
