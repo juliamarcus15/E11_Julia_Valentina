3736658f6cc08193f11e1fa3d35ffb944764f121
EthanJao <jao.ethan@gmail.com>
2026-03-06 08:44:14 -0800

    Update week6_engin11_lab_jupyternotebook_vf.py

--- week6_engin11_lab_jupyternotebook_vf.py
+++ week6_engin11_lab_jupyternotebook_vf.py
@@ -13,7 +13,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-indoor = pd.read_csv('lab5_trial5.csv')
+indoor = pd.read_csv('data/lab5_trial5.csv')
 indoor = indoor.iloc[15:].reset_index(drop=True) #drop first 15 rows because the trials are 2 seconds apart -- we cut the first 30 seconds we were not yet outside
 
 # Convert time to seconds since start
