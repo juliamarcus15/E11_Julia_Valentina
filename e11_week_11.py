27dd4e057e226e43510c76d60bd4c4c876fd408c
juliamarcus15 <jrmarcus@berkeley.edu>
2026-04-01 15:09:28 -0700

    Add import for CapeMCA from capemca module

--- e11_week_11.py
+++ e11_week_11.py
@@ -8,6 +8,8 @@ Original file is located at
 """
 
 import sys
+sys.path.append('/home/pi/cape_mca') # capemca.py directory
+from capemca import CapeMCA
 import time
 import numpy as np
 import matplotlib.pyplot as plt
